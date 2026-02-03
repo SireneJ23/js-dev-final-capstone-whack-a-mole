c4bda5e9fece137b144390a2b6c0a98f1d8f1866
SireneJ23 <114836820+SireneJ23@users.noreply.github.com>
2026-02-03 13:46:42 -0500

    Updated files to include mobile settings visibility

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "whack-a-mole-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./src/styles.css",
+  "./src/index.js",
+  "./manifest.webmanifest",
+
+  // Images
+  "./assets/park.png",
+  "./assets/hole.png",
+  "./assets/mole.png",
+  "./assets/wmole.png",
+  "./assets/mallet.png",
+  "./assets/tmole.png",
+
+  // Audio
+  "./assets/song.mp3",
+  "./assets/hit.mp3",
+  "./assets/victory.mp3",
+  "./assets/applause.mp3",
+  "./assets/defeat.mp3",
+
+  // Icons
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : null)),
+        ),
+      ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cached) => cached || fetch(event.request)),
+  );
+});
